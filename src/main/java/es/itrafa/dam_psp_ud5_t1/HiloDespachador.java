a3dd0f3ac3565556a83df6191532369fdf31a963
it-rafa <it-rafamartinez@outlook.es>
2022-04-28 10:51:47 +0200

    fixed message html and utf8

--- src/main/java/es/itrafa/dam_psp_ud5_t1/HiloDespachador.java
+++ src/main/java/es/itrafa/dam_psp_ud5_t1/HiloDespachador.java
@@ -8,6 +8,7 @@ package es.itrafa.dam_psp_ud5_t1;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
 
@@ -15,72 +16,78 @@ import java.net.Socket;
  *
  * @author Juan Morillo Fernandez
  */
-public class HiloDespachador extends Thread{
-    
-    
+public class HiloDespachador extends Thread {
+
     private Socket socket;
-    
-    public HiloDespachador (Socket client){
-    
-    this.socket= client;
+
+    public HiloDespachador(Socket client) {
+
+        this.socket = client;
     }
-    
-    public void run (){
-    
-    
-     String peticion;
-     String html;
-     try{
-         
-       //  String httpResponse = "HTTP/1.1 200 OK";
-        // socket.getOutputStream().write(httpResponse.getBytes("UTF-8"));
-               
-        InputStreamReader insSR = new InputStreamReader(socket.getInputStream());
-        BufferedReader bufLeer = new BufferedReader(insSR);
-        
-        PrintWriter print = new PrintWriter(socket.getOutputStream(),true);
-        
-        peticion = bufLeer.readLine();
-        
-        peticion= peticion.replaceAll(" ", "");
-        
-       if (peticion.startsWith("GET")){
-       
-            //extrae la subcadena entre GET y HTTP
-            peticion = peticion.substring(3,peticion.lastIndexOf("HTTP"));
 
-            if (peticion.length()== 0 || peticion.equals("/")){
+    public void run() {
 
-                html = Paginas.html_index;
-                print.println(Mensajes.lineaInicial_OK);
-                print.println(Paginas.primeraCabecera);
-                print.println("Content-Lenght: " + html.length() +1);
-                print.println("\n");
-                print.println(html);
-       }else if (peticion.equals("/contenido")){
-       
-           html= Paginas.html_contenido;
-           print.println(Mensajes.lineaInicial_OK);
-           print.println(Paginas.primeraCabecera);
-           print.println("Content-Lenght: " + html.length() +1);
-           print.println("\n");
-           print.println(html);
-       
-       }else {
-           html= Paginas.hrml_noEncontrado;
-           print.println(Mensajes.lineaInicial_NotFound);
-           print.println(Paginas.primeraCabecera);
-           print.println("Content-Lenght: " + html.length() +1);
-           print.println("\n");
-           print.println(html);
-       
-       }
-       
-       insSR.close();
-       bufLeer.close();     
-       socket.close();
-       }
-     }catch (IOException ex) { System.out.println("IOException capturada");}
-        
+        String peticion;
+        String html;
+        try {
+
+            //  String httpResponse = "HTTP/1.1 200 OK";
+            // socket.getOutputStream().write(httpResponse.getBytes("UTF-8"));
+            InputStreamReader insSR = new InputStreamReader(socket.getInputStream());
+            BufferedReader bufLeer = new BufferedReader(insSR);
+
+            PrintWriter print = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF8"), true);
+
+            peticion = bufLeer.readLine();
+
+            peticion = peticion.replaceAll(" ", "");
+
+            if (peticion.startsWith("GET")) {
+
+                //extrae la subcadena entre GET y HTTP
+                peticion = peticion.substring(3, peticion.lastIndexOf("HTTP"));
+
+                if (peticion.length() == 0 || peticion.equals("/")) {
+                    html = Paginas.html_index;
+                    print.println(Mensajes.lineaInicial_OK);
+                    print.println(Paginas.primeraCabecera);
+                    print.println("Content-Lenght: " + html.length() + 1);
+                    print.println("\n");
+                    print.println(html);
+                    
+                } else if (peticion.equals("/info")) {
+                    html = Paginas.html_info;
+                    print.println(Mensajes.lineaInicial_OK);
+                    print.println(Paginas.primeraCabecera);
+                    print.println("Content-Lenght: " + html.length() + 1);
+                    print.println("\n");
+                    print.println(html);
+
+                } else if (peticion.equals("/listado")) {
+                    html = Paginas.html_listado;
+                    print.println(Mensajes.lineaInicial_OK);
+                    print.println(Paginas.primeraCabecera);
+                    print.println("Content-Lenght: " + html.length() + 1);
+                    print.println("\n");
+                    print.println(html);
+
+                } else {
+                    html = Paginas.hrml_noEncontrado;
+                    print.println(Mensajes.lineaInicial_NotFound);
+                    print.println(Paginas.primeraCabecera);
+                    print.println("Content-Lenght: " + html.length() + 1);
+                    print.println("\n");
+                    print.println(html);
+
+                }
+
+                insSR.close();
+                bufLeer.close();
+                socket.close();
+            }
+        } catch (IOException ex) {
+            System.out.println("IOException capturada");
+        }
+
     }
 }
--- src/main/java/es/itrafa/dam_psp_ud5_t1/Paginas.java
+++ src/main/java/es/itrafa/dam_psp_ud5_t1/Paginas.java
@@ -1,5 +1,8 @@
 package es.itrafa.dam_psp_ud5_t1;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -7,36 +10,54 @@ package es.itrafa.dam_psp_ud5_t1;
  */
 public class Paginas {
 
+    private static DateTimeFormatter dtfHora = DateTimeFormatter.ofPattern("HH:mm:ss");
+    private static DateTimeFormatter dtfFecha = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    private static String txtHora = dtfHora.format(LocalDateTime.now());
+    private static String txtFecha = dtfFecha.format(LocalDateTime.now());
     public static final String primeraCabecera = "Content-Type:text/html;charset=UTF-8";
-    
-    public static final String html_index="<html><head>" +
-            "<title>Hola Mundo en HTML</title></head><body>" +
-"  <h2>mensaje de bienvenida con la hora actual</h2>" +
-"  <p><a href=\"info.html\">Inicio</a></p>" +
-"  <p><a href=\"/listado.html\">CSS Tutorial</a></p>" +
-            "</body></html>";
-    
-    public static final String html_contenido="<html>" +
-"  <head>" +
-"    <meta charset=\"utf-8\">" +
-"    <title>Inicio</title>" +
-"  </head>" +
-"  <body>" +
-"  <h2>otros</h2>" +
-"  <p><a href=\"info.html\">Inicio</a></p>" +
-"  <p><a href=\"/listado.html\">CSS Tutorial</a></p>" +
-"  </body>" +
-"</html>";
-    
-    public static final String hrml_noEncontrado = "<html>" +
-"  <head>" +
-"    <meta charset=\"utf-8\">" +
-"    <title>noEncontrado</title>" +
-"  </head>" +
-"  <body>" +
-"    <p>ERROR Pagina no encontrada</p>" +
-"  </body>" +
-"</html>";
 
+    public static final String html_index
+            = "<html>"
+            + "<head>"
+            + "   <meta charset=\"utf-8\">"
+            + "   <title>Inicio</title>"
+            + "</head>"
+            + "<body>"
+            + "  <h2>Bienvenido: Son las " + txtHora + " de " + txtFecha + "</h2>"
+            + "  <p><a href=\"/\">Inicio</a></p>"
+            + "  <p><a href=\"/info\">Info portal</a></p>"
+            + "  <p><a href=\"/listado\">Listado</a></p>"
+            + "</body></html>";
+
+    public static final String html_info
+            = "<html>"
+            + "<head>"
+            + "   <meta charset=\"utf-8\">"
+            + "   <title>Inicio</title>"
+            + "</head>"
+            + "<body>"
+            + "   <h2>info pendiente</h2>"
+            + "</body>" + "</html>";
+
+    public static final String html_listado
+            = "<html>"
+            + "<head>"
+            + "   <meta charset=\"utf-8\">"
+            + "   <title>Listado</title>"
+            + "</head>"
+            + "<body>"
+            + "   <h2>listado pendiente</h2>"
+            + "</body>" + "</html>";
+    public static final String hrml_noEncontrado
+            = "<html>"
+            + "<head>"
+            + "<meta charset=\"utf-8\">"
+            + "<title>Página no encontrada</title>"
+            + "</head>"
+            + "<body>"
+            + "<p>ERROR: Página no encontrada</p>"
+            + "<p>Ir a <a href=\"/\">Inicio</a></p>"
+            + "</body>"
+            + "</html>";
 
 }
